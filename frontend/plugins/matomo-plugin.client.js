3926501b7cfc32fa3304b8b7690ad27f45a23431
Michal Skop <michal.skop@kohovolit.eu>
2023-09-08 13:47:59 +0200

    switch to matomo

--- frontend/plugins/matomo-plugin.client.js
+++ frontend/plugins/matomo-plugin.client.js
@@ -3,8 +3,8 @@ import VueMatomo from 'vue-matomo'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueMatomo, {
-    host: 'https://piwik.kohovolit.eu',
-    siteId: 12, 
+    host: 'https://matomo.kohovolit.eu',
+    siteId: 2, 
     // Enables automatically registering pageviews on the router
     router: nuxtApp.$router,
     enableLinkTracking: true,
